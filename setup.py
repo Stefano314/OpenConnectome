4c0db64863ca50fb64c0ac4ad0b01440b4226889
Stefano <stefanobianchi314@gmail.com>
2023-11-29 22:34:09 +0100

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='OpenConnectome',
-    version='0.0.1',
+    version='0.0.11',
     description='Brain Graph Management and Numerical Model Simulations on Brains',
     long_description=long_description,
     long_description_content_type="text/markdown",
